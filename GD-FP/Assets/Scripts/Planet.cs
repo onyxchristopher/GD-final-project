dd5ac7d7f2e7ea2ea9cc3455c4f43ab7999c9b14
Onyx Christopher <onyx.t.christopher@gmail.com>
2024-12-09 17:21:08 -0500

    Added planets and adjusted movement curve

---
+++ GD-FP/Assets/Scripts/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private GameObject player;
+    private Vector2 dist;
+    [SerializeField] private float gravityScale;
+    private float gravitationalConstant = 50;
+
+    void Start() {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void FixedUpdate() {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        // get a vector between player and planet
+        dist = transform.position - player.transform.position;
+        Rigidbody2D playerRB = player.GetComponent<Rigidbody2D>();
+        playerRB.AddForce(dist * (gravitationalConstant * gravityScale / dist.sqrMagnitude));
+    }
+}
--- GD-FP/Assets/Scripts/PlayerMovement.cs
+++ GD-FP/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,21 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
+    
     private float fuel; // the current fuel level (depleted at 1/s)
     [SerializeField] private int maxFuel; // the maximum fuel
     private Slider fuelBarSlider;
 
     private GameController gameController;
 
-    private Rigidbody2D rb;
+    [HideInInspector] public Rigidbody2D rb;
+
+    [SerializeField] private float maxSpeed;
 
     private PlayerInput playerInput;
     private InputAction playerMove;
 
-    [SerializeField] private int moveSpeed;
+    [SerializeField] private float moveSpeed;
 
     [SerializeField] private GameObject blade;
     [SerializeField] private GameObject trap;
@@ -50,18 +53,27 @@ public class PlayerMovement : MonoBehaviour {
 
     void FixedUpdate() {
         Vector2 moveDir = playerMove.ReadValue<Vector2>();
-        float frameBalanceConst = Time.deltaTime;
+        Vector2 normalMoveDir = moveDir.normalized;
 
+        rb.AddForce(moveSpeed * normalMoveDir, ForceMode2D.Impulse);
+
+        Debug.Log(rb.velocity.magnitude);
+
+        float speedDifference = rb.velocity.magnitude - maxSpeed;
+        if (speedDifference > 0) {
+            rb.AddForce(-rb.velocity.normalized * speedDifference, ForceMode2D.Impulse);
+        }
+        
         if (moveDir != Vector2.zero) {
-            fuel -= frameBalanceConst; // spend 1 fuel/sec during movement
+            fuel -= Time.deltaTime; // spend 1 fuel/sec during movement
             fuelBarSlider.value = fuel;
             if (fuel <= 0) {
                 EventManager.PlayerDeath();
             }
         }
-
-        rb.velocity = moveSpeed * moveDir.normalized * frameBalanceConst * 50;
     }
-
-    
 }
+
+// F = a
+// F = v/s
+// F = 0.02v/0.02s
