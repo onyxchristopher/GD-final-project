220a5d77c57756d176b124bbfc5e8ba96a065d4e
Onyx Christopher <onyx.t.christopher@gmail.com>
2025-01-29 18:26:57 -0500

    Started boss 1 art

--- GD-FP/Assets/Scripts/EnemyScripts/PlanetguardBossEnemy.cs
+++ GD-FP/Assets/Scripts/EnemyScripts/PlanetguardBossEnemy.cs
@@ -7,7 +7,7 @@ using MEC;
 /*
 The planetguard has three states: IDLE, TRACK, and ATTACK.
 It starts in IDLE, and when its trigger is entered by the player, moves to TRACK.
-In TRACK, it rotates so its turret is facing the player. When the player is
+In TRACK, it rotates so one of its turrets is facing the player. When the player is
 in front of the turret, it moves to ATTACK. It moves back to TRACK when the player
 gets out of the line of fire, and back to IDLE if the player moves out of range.
 
@@ -17,12 +17,13 @@ when this happens. Any crack becomes invulnerable when damaged.
 */
 
 public class PlanetguardBossEnemy : Enemy {
-    private GameController gameController;
-    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Vector3 rotationVector;
     private Rigidbody2D playerRB;
     private Vector2 spawnpoint;
-    private GameObject turret;
-    private PlanetguardTurretEnemy turretControl;
+    private GameObject turret1;
+    private GameObject turret2;
+    private PlanetguardTurretEnemy turretControl1;
+    private PlanetguardTurretEnemy turretControl2;
     private Damageable damageable;
     [SerializeField] private string bossName;
 
@@ -35,27 +36,35 @@ public class PlanetguardBossEnemy : Enemy {
     void Start() {
         playerRB = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
         spawnpoint = new Vector2(transform.position.x, transform.position.y);
-        turret = transform.GetChild(1).gameObject;
-        turretControl = turret.GetComponent<PlanetguardTurretEnemy>();
+        turret1 = transform.GetChild(1).gameObject;
+        turret2 = transform.GetChild(2).gameObject;
+        turretControl1 = turret1.GetComponent<PlanetguardTurretEnemy>();
+        turretControl2 = turret2.GetComponent<PlanetguardTurretEnemy>();
         gameObject.GetComponent<Damageable>().enemy = this;
-        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
     }
 
     private void TrackLoop() {
         if (state != State.TRACK) {
             return;
         }
-        Timing.RunCoroutine(_RotatePlanetguard(), Segment.FixedUpdate);
+        if (gameObject != null && gameObject.activeInHierarchy) {
+            Timing.RunCoroutine(_RotatePlanetguard(), Segment.FixedUpdate);
+        }
     }
 
     private IEnumerator<float> _RotatePlanetguard() {
         Vector2 dirToPlayer = playerRB.position - spawnpoint;
-        if (Vector2.SignedAngle(-transform.up, dirToPlayer) < 0) {
-            transform.Rotate(new Vector3(0, 0, -rotationSpeed));
+
+        // rotate the closest turret towards the player
+        float angle = Vector2.SignedAngle(transform.up, dirToPlayer);
+        if ((angle > 0 && angle <= 90) || (angle > -180 && angle <= -90)) {
+            transform.Rotate(rotationVector);
         } else {
-            transform.Rotate(new Vector3(0, 0, rotationSpeed));
+            transform.Rotate(-rotationVector);
         }
-        turretControl.location = new Vector2(turret.transform.position.x, turret.transform.position.y);
+        turretControl1.location = (Vector2) turret1.transform.position;
+        turretControl2.location = (Vector2) turret2.transform.position;
+
         yield return Timing.WaitForOneFrame;
         TrackLoop();
     }
